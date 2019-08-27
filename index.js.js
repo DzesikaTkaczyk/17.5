1f54ac1e7ccc2a0f713664074192b21d20f56d98
DzesikaTkaczyk <50298851+DzesikaTkaczyk@users.noreply.github.com>
2019-08-27 19:05:14 +0200

    Update index.js.js

--- index.js.js
+++ index.js.js
@@ -4,25 +4,25 @@ var OSinfo = require('./modules/OSinfo');
 process.stdin.setEncoding('utf-8');
 
 process.stdin.on('readable', function() {
-    var input = process.stdin.read();
+	var input = process.stdin.read();
 
-    if (!input) {
-        return
-    }
+	if (!input) {
+		return
+	}
 
 	var instruction = input.toString().trim();
 	switch (instruction) {
     	case '/exit':
     		process.stdout.write('Quitting app!\n');
     		process.exit();
-            break;
+		break;
         case '/sayhello':
-            process.stdout.write('hello!\n');
-            break;
+		process.stdout.write('hello!\n');
+		break;
          case '/getOSinfo':
-            OSinfo.print();
-            break;
+		OSinfo.print();
+		break;
     	default:
     		process.stderr.write('Wrong instruction!\n')
-    }
+	}
 });
